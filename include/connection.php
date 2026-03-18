4f583e850654053ddbae69013f7b7864622b7fe6
Jelly Concepcion <jelsdizoncon@gmail.com>
2026-03-18 09:23:59 +0800

    created admin with password 1234 on xampp shell and exported new database

--- include/connection.php
+++ include/connection.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = mysqli_connect("10.50.128.29","root","","bayungporacarchive_db");
+$conn = mysqli_connect("localhost","admin","1234","bayungporacarchive_db");
 
 if(!$conn){
 	die("Connection error: " . mysqli_connect_error());	
