fb1dbb596e5ada054b1426ee16a1d8e8d8dd1585
Joys <a.internfive@gmail.com>
2026-03-18 14:27:07 +0800

    designed the index.php

--- include/connection.php
+++ include/connection.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = mysqli_connect("10.50.128.29","root","","bayungporacarchive_db");
+$conn = mysqli_connect("10.50.139.159","root","","bayungporacarchive_db");
 
 if(!$conn){
 	die("Connection error: " . mysqli_connect_error());	
